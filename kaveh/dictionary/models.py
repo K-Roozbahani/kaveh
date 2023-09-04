d9a72769529d847c52a0ceeaa9fe229d131228a8
k-roozbahani <kavehrozbah@gmail.com>
2023-09-04 20:58:53 +0330

    add first ditionary models

--- kaveh/dictionary/models.py
+++ kaveh/dictionary/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class MyAbstractModel(models.Model):
+    created_time = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
+
+
+class WordAbstractModel(MyAbstractModel):
+    title = models.CharField(max_length=255, unique=True)
+    mean = models.TextField(blank=True, null=True)
+
+    class Meta:
+        abstract = True
+
+
+class TypeWordModel(MyAbstractModel):
+    name = models.CharField(max_length=64, unique=True)
+
+
+class WordModel(WordAbstractModel):
+    origin = models.ForeignKey("WordModel", on_delete=models.CASCADE, related_name="branches")
+    type = models.ManyToManyField("TypeWordModel", related_name="words", )
+
+
+class ExtraMeanModel(WordAbstractModel):
+    title = models.ForeignKey("WordModel", on_delete=models.CASCADE, related_name="extra_means")
