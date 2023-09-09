c3b81447b35aedc2d5b41da6f2e0881d50bfc3ae
k-roozbahani <kavehrozbah@gmail.com>
2023-09-09 11:28:30 +0330

    add word retrieve url

--- kaveh/dictionary/urls.py
+++ kaveh/dictionary/urls.py
@@ -3,4 +3,5 @@ from .views import WordRetrieveView
 
 urlpatterns = [
     path('add/', WordRetrieveView.as_view(), name="add_word"),
+    path('<int:pk>/', WordRetrieveView.as_view(), name="retrieve_word"),
 ]
